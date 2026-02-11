7534abaea649782427f28a39ddd00ddd002ad874
AgniaNazwan <247006111076@student.unsil.ac.id>
2026-02-11 09:56:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const text = "Agnia Nazwan Rahmatunnisa";
-  const speed = 120;
-  let index = 0;
+const text = "Agnia Nazwan Rahmatunnisa";
+const speed = 120;
+let index = 0;
 
-  const typing = document.getElementById("typing-name");
-  const btn = document.getElementById("btn");
-
-  function typeEffect() {
-    if (typing && index < text.length) {
-      typing.textContent += text.charAt(index);
-      index++;
-      setTimeout(typeEffect, speed);
-    }
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typing-name").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
   }
+}
 
-  typeEffect();
+window.addEventListener("load", typeEffect);
 
-  if (btn) {
-    btn.addEventListener("click", function () {
-      window.open(
-        "https://wa.me/6283101609541?text=Halo%20Agnia,%20saya%20melihat%20portofolio%20Anda",
-        "_blank"
-      );
-    });
-  }
+// === TAMBAHAN WA  ===
+document.getElementById("btn").addEventListener("click", function () {
+  window.open(
+    "https://wa.me/6283101609541?text=Halo%20Agnia,%20saya%20melihat%20portofolio%20Anda",
+    "_blank",
+  );
 });
